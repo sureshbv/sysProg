6db3da1ad6e023945a76d404928b44de848e44f1
Suresh BV <sureshkumar.bv@gmail.com>
2017-12-05 18:15:09 +0000

    first commit

---
+++ slices.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printSlice(x []int) {
+
+	for _, number := range x {
+		fmt.Printf("%d ", number)
+	}
+	fmt.Println()
+}
+
+func change(x []int) {
+	x[3] = -2
+}
+
+
+func main() {
+	aSlice := []int{ 0, 1, -1, 4, 5, 6}
+	fmt.Printf("Before change: ")
+	printSlice(aSlice)
+	change(aSlice)
+	fmt.Printf("After change: ")
+	printSlice(aSlice)
+	fmt.Printf("Before. Cap: %d, length: %d \n", cap(aSlice), len(aSlice))
+	aSlice = append(aSlice, -100)
+	printSlice(aSlice)
+	fmt.Printf("After. Cap: %d, length: %d \n", cap(aSlice), len(aSlice))
+	
+	anotherSlice := make([]int, 4)
+	printSlice(anotherSlice)
+
+}
